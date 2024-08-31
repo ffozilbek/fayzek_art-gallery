ddc52340fce17e240e68ead4b03b99de63417df6
ffozilbek <fozilbekfayzullayev05@gmail.com>
2024-08-31 22:42:50 +0500

    fix loader site

--- js/script.js
+++ js/script.js
@@ -1,4 +1,14 @@
 window.addEventListener("DOMContentLoaded", ()=> {
+  document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector(".loader").style.visibility = "visible";
+    } else {
+        document.querySelector(".loader").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
+
 
   const elHeader = document.querySelector(".header");
     window.addEventListener("scroll", ()=> {
