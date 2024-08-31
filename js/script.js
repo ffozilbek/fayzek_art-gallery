11f27ecc9f52bfd43bcc783a1bbad2062c7df717
ffozilbek <fozilbekfayzullayev05@gmail.com>
2024-08-31 22:32:53 +0500

    images optimizer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
-window.addEventListener("load", ()=> {
-  const elLoader = document.querySelector(".loader");
-  elLoader.style.display = "none";
-})
-
 window.addEventListener("DOMContentLoaded", ()=> {
-    const elHeader = document.querySelector(".header");
+
+  const elHeader = document.querySelector(".header");
     window.addEventListener("scroll", ()=> {
         if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
             elHeader.classList.add("scroll");
