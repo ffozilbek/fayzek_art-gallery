991d36b3c0eb710557e554a282a19216e04267f7
ffozilbek <fozilbekfayzullayev05@gmail.com>
2024-08-31 23:04:46 +0500

    fixed 4

--- js/script.js
+++ js/script.js
@@ -1,15 +1,18 @@
-window.addEventListener("DOMContentLoaded", ()=> {
-  document.onreadystatechange = function() {
-    if (document.readyState !== "complete") {
-        document.querySelector("body").style.visibility = "hidden";
-        document.querySelector(".loader").style.visibility = "visible";
-    } else {
-        document.querySelector(".loader").style.display = "none";
-        document.querySelector("body").style.visibility = "visible";
-    }
+document.onreadystatechange = function() {
+  if (document.readyState !== "complete") {
+      document.querySelector(".loader").style.visibility = "visible";
+  } else {
+      document.querySelector(".loader").style.display = "none";
+  }
 };
 
+setTimeout(() => {
+  document.querySelector(".loader").style.display = "none";
+}, 10000);
 
+
+
+window.addEventListener("DOMContentLoaded", ()=> {
   const elHeader = document.querySelector(".header");
     window.addEventListener("scroll", ()=> {
         if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
