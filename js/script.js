184c7fa37a235bd9c099c9092dcb638713dd103c
ffozilbek <fozilbekfayzullayev05@gmail.com>
2024-08-31 22:00:06 +0500

    fixed loader

--- js/script.js
+++ js/script.js
@@ -1,15 +1,10 @@
+window.addEventListener("load", ()=> {
+  const elLoader = document.querySelector(".loader");
+  elLoader.style.display = "none";
+})
+
 window.addEventListener("DOMContentLoaded", ()=> {
-    const elHeader = document.querySelector(".header"),
-        elLoader = document.querySelector(".loader");
-
-    window.addEventListener("load", ()=> {
-      setTimeout(() => {
-        elLoader.classList.remove("active");
-      }, 5000);
-    })
-
-    elLoader.classList.add("active");
-
+    const elHeader = document.querySelector(".header");
     window.addEventListener("scroll", ()=> {
         if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
             elHeader.classList.add("scroll");
